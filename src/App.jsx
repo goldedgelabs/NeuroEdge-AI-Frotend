dccd408823d8a106b1e91e2e77b13016e9dd71ef
goldedgelabs <goldedgelabs@gmail.com>
2025-11-19 01:06:14 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useContext } from "react";
 
-import SplashScreen from "./pages/SplashScreen";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
+import { ChatProvider } from "./context/ChatContext";
+import { AuthProvider } from "./context/AuthContext";
+import { AppProvider } from "./context/AppContext";
 
-import AppLayout from "./layout/AppLayout";
+import Sidebar from "./components/layout/Sidebar";
+import TopBar from "./components/layout/TopBar";
+import MessageList from "./components/chat/MessageList";
+import ChatInput from "./components/chat/ChatInput";
+import ReasoningDrawer from "./components/layout/ReasoningDrawer";
+
+import "./styles/globals.css";
+import "./styles/layout.css";
+import "./styles/chatinput.css";
+import "./styles/menu.css";
+import "./styles/modal.css";
+import "./styles/animations.css";
+
+function Shell() {
+  const { messages, sendMessage, isTyping } = useContext(require("./context/ChatContext").ChatContext);
+
+  return (
+    <div className="app-shell">
+      <Sidebar />
+
+      <div className="main-column">
+        <TopBar />
+
+        <div className="chat-area">
+          <MessageList messages={messages} isTyping={isTyping} />
+        </div>
+
+        <ChatInput onSendText={sendMessage} />
+      </div>
+
+      <ReasoningDrawer />
+    </div>
+  );
+}
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Loading screen */}
-        <Route path="/" element={<SplashScreen />} />
-
-        {/* Authentication */}
-        <Route path="/login" element={<Login />} />
-
-        {/* Main Chat Layout */}
-        <Route path="/app" element={<AppLayout />} />
-
-        {/* Profile */}
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <AppProvider>
+        <ChatProvider>
+          <Shell />
+        </ChatProvider>
+      </AppProvider>
+    </AuthProvider>
   );
 }
